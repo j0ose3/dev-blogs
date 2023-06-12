8abdd03710b8c47cda314ac9b93867679d8fdb92
unknown <j0ose3x7@gmail.com>
2023-06-11 23:34:08 -0400

    Started Setup for project

---
+++ pages/404.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/NotFound.module.css';
+
+const NotFound = () => {
+    return (
+      <>
+        <div className={styles.container}>404 Not Found</div>
+      </>
+    );
+  };
+  
+  export default NotFound;
---
+++ pages/contact.tsx
@@ -0,0 +1,12 @@
+
+
+
+const Contact = () => {
+  return (
+    <>
+      <div>Hello Contact</div>
+    </>
+  );
+};
+
+export default Contact;
