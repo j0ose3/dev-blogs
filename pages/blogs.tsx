6e804167f9dcde75e43fa4493ea830010e493fa6
unknown <j0ose3x7@gmail.com>
2023-06-17 22:51:35 -0400

    Setup static page for Blogs

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,14 +1,32 @@
 import BlogCard from "@/components/BlogCard";
-import { NextPage } from "next";
+import { InferGetStaticPropsType, NextPage } from "next";
+import { useEffect, useState } from "react";
 
-interface Props {}
+interface PostApiResponse {
+  postInfo: {
+    title: string;
+    slug: string;
+    meta: string
+  }[];
+}
 
-const Blogs: NextPage<Props> = () => {
+export const getStaticProps = async () => {
+  
+  const {postInfo}:PostApiResponse =  await fetch('http://localhost:3000/api/posts').then(data => data.json());
+
+  return {
+    props: {posts: postInfo}
+  }
+}
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const Blogs: NextPage<Props> = ({posts}) => {
+
+
   return (
     <div className="text-gray-400  p-5 max-w-3xl mx-auto space-y-5">
-      <BlogCard title="Title" description="Description"/>
-      <BlogCard title="Title" description="Description"/>
-      <BlogCard title="Title" description="Description"/>
+      {posts.map(post => <BlogCard key={post.title} title={post.title} description={post.meta}/>)}
     </div>
   );
 };
