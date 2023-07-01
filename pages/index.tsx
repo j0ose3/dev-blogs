b30c198cd2981dd9e1558885cd07f42a1f86b07c
unknown <j0ose3x7@gmail.com>
2023-06-30 23:37:55 -0400

    We just changed homepage to blogs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,36 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import BlogCard from "@/components/BlogCard";
+import { readPostsInfo } from "@/lib/helper";
+import { PostApiResponse } from "@/utils/types";
+import { InferGetStaticPropsType, NextPage } from "next";
 
-const Home: NextPage = () => {
+export const getStaticProps = async () => {
+  //   const { postInfo }: PostApiResponse = await fetch(
+  //     "http://localhost:3000/api/posts"
+  //   ).then((data) => data.json());
+
+  const postInfo:PostApiResponse = readPostsInfo();
+
+  return {
+    props: { posts: postInfo },
+  };
+};
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Blogs: NextPage<Props> = ({ posts }) => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="text-gray-400  p-5 max-w-3xl mx-auto space-y-5">
+      {posts.map((post) => (
+        <BlogCard
+          key={post.slug}
+          title={post.title}
+          description={post.meta}
+          slug={post.slug}
+        />
+      ))}
+    </div>
   );
 };
 
-export default Home;
+export default Blogs;
 
